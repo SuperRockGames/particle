b1318a76ccd0cd8898d73aa687f07143d808fa05
Super Rock Games <30961532+SuperRockGames@users.noreply.github.com>
2017-08-25 15:11:15 +0100

    Revert color

--- src/js/app.js
+++ src/js/app.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "type": "polygon",
         "stroke": {
           "width": 0,
-          "color": "#0486DB"
+          "color": "#000000"
         },
         "polygon": {
           "nb_sides": 5
